347c8e2e10fb376b4eed8227e7025f47c0d42bef
Oscar Goldman <damian.woydaa@gmail.com>
2026-01-02 20:32:28 +0100

    Fix SQL insert statement formatting

---
+++ scripts/CL_erp_px_cat_g1v2.sql
@@ -0,0 +1,19 @@
+-- ===========================================
+-- CLEAN & LOAD 
+-- bronze.erp_px_cat_g1v2
+-- OUTPUT -> silver.erp_px_cat_g1v2
+-- ===========================================
+
+INSERT INTO silver.erp_px_cat_g1v2 
+(
+  id, 
+	cat, 
+	subcat, 
+	maintenance
+)
+SELECT
+	id, 
+	cat, 
+	subcat, 
+	maintenance
+FROM bronze.erp_px_cat_g1v2
