cb0bff2a285d57760fa8cf8a33b4b906dbe17dc4
Oscar Goldman <damian.woydaa@gmail.com>
2026-01-03 17:59:02 +0100

    upagrade

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -8,52 +8,65 @@ Script Purpose:
 	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
+
+DROP TABLE IF EXISTS bronze.crm_cust_info CASCADE;
+
 CREATE TABLE IF NOT EXISTS bronze.crm_cust_info (
 	cst_id 					INTEGER,
 	cst_key 				VARCHAR(50),
 	cst_firstname 			VARCHAR(50),
 	cst_lastname 			VARCHAR(50),
-	cst_material_status 	VARCHAR(50),
+	cst_marital_status	 	VARCHAR(50),
 	cst_gender 				VARCHAR(50),
 	cst_create_date 		DATE
 );
 
+DROP TABLE IF EXISTS bronze.crm_prd_info CASCADE;
+
 CREATE TABLE IF NOT EXISTS bronze.crm_prd_info (
-	prd_id 			INTEGER,
-	prd_key 		VARCHAR(50),
-	prd_nm 			VARCHAR(50),
-	prd_cost 		INTEGER,
-	prd_line 		VARCHAR(50),
-	prd_start_dt 	DATE,
-	prd_end_dt 		DATE
+	prd_id 					INTEGER,
+	prd_key 				VARCHAR(50),
+	prd_nm 					VARCHAR(50),
+	prd_cost 				INTEGER,
+	prd_line 				VARCHAR(50),
+	prd_start_dt 			DATE,
+	prd_end_dt 				DATE
 );
+
+DROP TABLE IF EXISTS bronze.crm_sales_details CASCADE;
 
 CREATE TABLE IF NOT EXISTS bronze.crm_sales_details (
-	sls_ord_num 	VARCHAR(50),
-	sls_prd_key 	VARCHAR(50),
-	sls_cust_id 	INTEGER,
-	sls_order_dt 	INTEGER,
-	sls_ship_dt 	INTEGER,
-	sls_due_dt 		INTEGER,
-	sls_sales 		INTEGER,
-	sls_quantity 	INTEGER,
-	sls_price 		INTEGER
+	sls_ord_num 			VARCHAR(50),
+	sls_prd_key 			VARCHAR(50),
+	sls_cust_id 			INTEGER,
+	sls_order_dt 			INTEGER,
+	sls_ship_dt 			INTEGER,
+	sls_due_dt 				INTEGER,
+	sls_sales 				INTEGER,
+	sls_quantity 			INTEGER,
+	sls_price 				INTEGER
 );
+
+DROP TABLE IF EXISTS bronze.erp_cust_az12 CASCADE;
 
 CREATE TABLE IF NOT EXISTS bronze.erp_cust_az12 (
-	cid 	VARCHAR(50),
-	bdate 	DATE,
-	gen 	VARCHAR(50)
+	cid 					VARCHAR(50),
+	bdate 					DATE,
+	gen 					VARCHAR(50)
 );
+
+DROP TABLE IF EXISTS bronze.erp_loc_a101 CASCADE;
 
 CREATE TABLE IF NOT EXISTS bronze.erp_loc_a101 (
-	cid 	VARCHAR(50),
-	cntry 	VARCHAR(50)
+	cid 					VARCHAR(50),
+	cntry 					VARCHAR(50)
 );
 
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2 CASCADE;
+
 CREATE TABLE IF NOT EXISTS bronze.erp_px_cat_g1v2 (
-	id 			VARCHAR(50),
-	cat 		VARCHAR(50),
-	subcat 		VARCHAR(50),
-	maintenance VARCHAR(50)
+	id 						VARCHAR(50),
+	cat 					VARCHAR(50),
+	subcat 					VARCHAR(50),
+	maintenance				VARCHAR(50)
 );
